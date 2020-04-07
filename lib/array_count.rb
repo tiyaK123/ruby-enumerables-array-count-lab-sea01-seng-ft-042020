696ac546e0e68cac283cedc00b8bd9d762872529
tiya adugna <tiyakefalee@gmail.com>
2020-04-07 07:22:23 +0000

    Automatically backed up by Learn

--- lib/array_count.rb
+++ lib/array_count.rb
@@ -3,7 +3,7 @@ def count_strings(array)
   count = 0 
   num = 0 
   while array.length > count do 
-     if array[count].to_a? :String 
+     if array[count].to_s? :String 
      num += 1 
    end
      array[count]
